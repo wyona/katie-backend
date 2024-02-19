9ec1c10dbf6dab29636dd28a09aed063c17a3526
Michael Wechner <michael.wechner@wyona.com>
2024-02-19 09:59:15 +0100

    comment and hint improved

--- src/main/java/com/wyona/katie/integrations/slack/SlackMessageSender.java
+++ src/main/java/com/wyona/katie/integrations/slack/SlackMessageSender.java
@@ -592,7 +592,7 @@ public class SlackMessageSender extends CommonMessageSender  {
         elementAskedQuestionNode.put("type", "plain_text_input");
         elementAskedQuestionNode.put("action_id", "single_line_input");
 
-        // TODO: Set askedQuestion as actual value and not just as placeholder
+        // TODO: Set askedQuestion as actual value and not just as placeholder and update hint below accordingly
         ObjectNode placeholderAskedQuestionNode = mapper.createObjectNode();
         elementAskedQuestionNode.put("placeholder", placeholderAskedQuestionNode);
         placeholderAskedQuestionNode.put("type", FORMAT_PLAIN_TEXT);
@@ -606,7 +606,7 @@ public class SlackMessageSender extends CommonMessageSender  {
         ObjectNode hintNode = mapper.createObjectNode();
         inputAskedQuestionBlockNode.put("hint", hintNode);
         hintNode.put("type", FORMAT_PLAIN_TEXT);
-        hintNode.put("text", "Please enter question as asked or feel free to enter a modified version: " + askedQuestion); // TODO
+        hintNode.put("text", "Please enter asked question or feel free to enter a modified version: " + askedQuestion);
 
         // INFO: Better answer block
         ObjectNode inputBetterAnswerBlockNode = mapper.createObjectNode();
