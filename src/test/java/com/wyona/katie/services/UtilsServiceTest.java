0a81d5a510d396b70058c88c8c3f0f8874c4f6da
Michael Wechner <michael.wechner@wyona.com>
2024-02-29 15:34:13 +0100

    test utils started

---
+++ src/test/java/com/wyona/katie/services/UtilsServiceTest.java
@@ -0,0 +1,39 @@
+package com.wyona.katie.services;
+
+import com.wyona.katie.ai.models.FloatVector;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class UtilsServiceTest {
+
+    /**
+     * TODO
+     */
+    @Test
+    void getCentroid() {
+        FloatVector a = new FloatVector(2);
+        a.set(0, 0);
+        a.set(1, 3);
+        FloatVector b = new FloatVector(2);
+        b.set(0, 2);
+        b.set(1, 5);
+        FloatVector c = new FloatVector(2);
+        c.set(0, 4);
+        c.set(1, 1);
+        List<FloatVector> vectors = new ArrayList<>();
+        vectors.add(a);
+        vectors.add(b);
+        vectors.add(c);
+        FloatVector centroid = UtilsService.getCentroid(vectors.toArray(new FloatVector[0]));
+        log.info("Centroid: " + centroid);
+        assertTrue(centroid.getValues()[0] == 2);
+        assertTrue(centroid.getValues()[1] == 3);
+    }
+}
