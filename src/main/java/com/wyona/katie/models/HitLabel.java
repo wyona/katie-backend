510ba1dedb320a32d1044267f292a158068fb833
Michael Wechner <michael.wechner@wyona.com>
2024-03-01 11:34:20 +0100

    hit label introduced

---
+++ src/main/java/com/wyona/katie/models/HitLabel.java
@@ -0,0 +1,43 @@
+package com.wyona.katie.models;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ *
+ */
+@Slf4j
+public class HitLabel {
+
+    private Classification label;
+    private double score;
+
+    // INFO: Default constructor is necessary, because otherwise a 400 is generated when using @RequestBody (see https://stackoverflow.com/questions/27006158/error-400-spring-json-requestbody-when-doing-post)
+    /**
+     *
+     */
+    public HitLabel() {
+    }
+
+    /**
+     * @param label Label / classification
+     * @param score Confidence score, that label is right
+     */
+    public HitLabel(Classification label, double score) {
+        this.label = label;
+        this.score = score;
+    }
+
+    /**
+     *
+     */
+    public Classification getLabel() {
+        return label;
+    }
+
+    /**
+     *
+     */
+    public double getScore() {
+        return score;
+    }
+}
