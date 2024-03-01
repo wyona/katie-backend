9995ffb09d9c1b0114adb9a5d98776f7c7747b58
Michael Wechner <michael.wechner@wyona.com>
2024-03-01 15:02:32 +0100

    classification id added

--- src/main/java/com/wyona/katie/models/Classification.java
+++ src/main/java/com/wyona/katie/models/Classification.java
@@ -3,11 +3,21 @@ package com.wyona.katie.models;
 public class Classification {
 
     private String term;
+    private Integer id;
 
     /**
      *
      */
     public Classification() {
+    }
+
+    /**
+     * @param term Text, e.g. "Managed Device Services, MacOS Clients"
+     * @param id Id, e.g. 15
+     */
+    public Classification(String term, Integer id) {
+        this.term = term;
+        this.id = id;
     }
 
     /**
@@ -22,5 +32,19 @@ public class Classification {
      */
     public String getTerm() {
         return term;
+    }
+
+    /**
+     *
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     */
+    public Integer getId() {
+        return id;
     }
 }
