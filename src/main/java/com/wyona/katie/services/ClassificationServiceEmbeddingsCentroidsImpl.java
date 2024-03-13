a8d9b75ee0dfb4eda1b7fa696eeae3b9a697a718
Michael Wechner <michael.wechner@wyona.com>
2024-03-13 09:26:36 +0100

    logging improved

--- src/main/java/com/wyona/katie/services/ClassificationServiceEmbeddingsCentroidsImpl.java
+++ src/main/java/com/wyona/katie/services/ClassificationServiceEmbeddingsCentroidsImpl.java
@@ -53,8 +53,10 @@ public class ClassificationServiceEmbeddingsCentroidsImpl implements Classificat
         // TODO: Combine both search results!
         // TODO: centroids can be very close to each other, which means a query vector can be very close to a a wrong centroid and at the same time very close to an invidual sample vector associated with the correct centroid.
         if (true) {
+            log.info("Predict labels by finding similar samples ...");
             return searchSimilarSampleVectors(domain, queryVector);
         } else {
+            log.info("Predict labels by finding similar centroids ...");
             return searchSimilarCentroidVectors(domain, queryVector);
         }
     }
