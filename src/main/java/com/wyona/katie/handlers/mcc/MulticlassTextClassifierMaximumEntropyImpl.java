a5c1d458b7c37333584b9d15880282b299186264
Michael Wechner <michael.wechner@wyona.com>
2024-03-18 19:39:24 +0100

    two multiclass text classifier implementations added

---
+++ src/main/java/com/wyona/katie/handlers/mcc/MulticlassTextClassifierMaximumEntropyImpl.java
@@ -0,0 +1,28 @@
+package com.wyona.katie.handlers.mcc;
+
+import com.wyona.katie.models.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * https://opennlp.apache.org/docs/1.7.2/manual/opennlp.html#tools.doccat
+ * https://blog.datumbox.com/machine-learning-tutorial-the-max-entropy-text-classifier/
+ */
+@Slf4j
+@Component
+public class MulticlassTextClassifierMaximumEntropyImpl implements MulticlassTextClassifier {
+
+    /**
+     * @see com.wyona.katie.handlers.mcc.MulticlassTextClassifier#predictLabels(Context, String) 
+     */
+    public HitLabel[] predictLabels(Context domain, String text) throws Exception {
+        return null;
+    }
+
+    /**
+     * @see com.wyona.katie.handlers.mcc.MulticlassTextClassifier#train(Context, TextSample[])
+     */
+    public void train(Context domain, TextSample[] samples) throws Exception {
+        // TODO
+    }
+}
