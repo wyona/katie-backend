16337be34c8365623fa074df0281293d2cb04f43
Michael Wechner <michael.wechner@wyona.com>
2024-03-22 00:11:47 +0100

    embedding value type introduced

---
+++ src/main/java/com/wyona/katie/models/EmbeddingValueType.java
@@ -0,0 +1,9 @@
+package com.wyona.katie.models;
+
+/**
+ * See for example https://www.elastic.co/search-labs/blog/articles/scalar-quantization-101
+ */
+public enum EmbeddingValueType {
+    float32,
+    int8
+}
