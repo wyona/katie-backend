f0b48a368a748a5d003fb4da90074117c727fb26
Michael Wechner <michael.wechner@wyona.com>
2024-03-22 10:45:30 +0100

    test byte vector

--- src/test/java/com/wyona/katie/services/UtilsServiceTest.java
+++ src/test/java/com/wyona/katie/services/UtilsServiceTest.java
@@ -1,11 +1,13 @@
 package com.wyona.katie.services;
 
+import com.wyona.katie.models.ByteVector;
 import com.wyona.katie.models.FloatVector;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -43,8 +45,27 @@ class UtilsServiceTest {
 
         FloatVector d = a.clone();
         d.add(b);
-        log.info("a + b: " + d);
-        assertTrue(d.getValues()[0] == 2);
-        assertTrue(d.getValues()[1] == 8);
+        log.info("Float vector addition: a + b = " + d);
+        FloatVector result = new FloatVector(2);
+        result.set(0, 2);
+        result.set(1, 8);
+        assertTrue(Arrays.equals(d.getValues(), result.getValues()));
+
+        ByteVector bv_a = new ByteVector(2);
+        bv_a.set(0, (byte) -5);
+        bv_a.set(1, (byte) 4);
+
+        ByteVector bv_b = new ByteVector(2);
+        bv_b.set(0, (byte) 3);
+        bv_b.set(1, (byte) 7);
+
+        ByteVector bv_result = new ByteVector(2);
+        bv_result.set(0, (byte) -2);
+        bv_result.set(1, (byte) 11);
+        log.info("Expected byte vector addition result: " + bv_result);
+
+        bv_a.add(bv_b);
+        log.info("Byte vector addition: a + b = " + bv_a);
+        assertTrue(Arrays.equals(bv_a.getValues(), bv_result.getValues()));
     }
 }
