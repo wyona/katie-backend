04a5b444ff9920e7acb26a9174ae60305850d805
Michael Wechner <michael.wechner@wyona.com>
2024-04-21 13:47:17 +0200

    obsolete line removed

--- src/main/java/com/wyona/katie/config/SecurityConfig.java
+++ src/main/java/com/wyona/katie/config/SecurityConfig.java
@@ -72,7 +72,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 //.antMatchers("/h2-console/**").permitAll()
-                .antMatchers(GET, "/api/v1/summary/current").hasRole(ADMIN_ROLE)
                 .antMatchers(GET, "/api/v1/question/resubmitted/*").hasAnyRole(USER_ROLE, ADMIN_ROLE)
                 .antMatchers(PUT, "/api/v1/question/resubmitted/*").hasAnyRole(USER_ROLE, ADMIN_ROLE)
                 .antMatchers(DELETE, "/api/v1/question/resubmitted/*").hasAnyRole(USER_ROLE, ADMIN_ROLE)
