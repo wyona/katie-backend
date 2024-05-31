e42bfacb89aac3eebeb43cdc8af6f34713e3a63d
Michael Wechner <michael.wechner@wyona.com>
2024-05-31 15:20:34 +0200

    get GenAI model

--- src/main/java/com/wyona/katie/services/ContextService.java
+++ src/main/java/com/wyona/katie/services/ContextService.java
@@ -39,6 +39,15 @@ import javax.servlet.http.HttpServletRequest;
 @DependsOn("IAMService")
 public class ContextService {
 
+    @Value("${mistral.ai.completion.model}")
+    private String mistralAIModel;
+
+    @Value("${openai.generate.model}")
+    private String openAIModel;
+
+    @Value("${ollama.completion.model}")
+    private String ollamaModel;
+
     @Value("${question.answer.implementation}")
     private DetectDuplicatedQuestionImpl defaultDetectDuplicatedQuestionImpl;
 
@@ -1872,6 +1881,26 @@ public class ContextService {
         } else {
             return null;
         }
+    }
+
+    /**
+     *
+     */
+    public String getCompletionModel(CompletionImpl generateImpl) {
+        String model = null;
+        if (generateImpl.equals(CompletionImpl.ALEPH_ALPHA)) {
+            model = "luminous-base"; // TODO: Make configurable
+        } else if (generateImpl.equals(CompletionImpl.OPENAI)) {
+            model = openAIModel;
+        } else if (generateImpl.equals(CompletionImpl.MISTRAL_AI)) {
+            model = mistralAIModel;
+        } else if (generateImpl.equals(CompletionImpl.MISTRAL_OLLAMA)) {
+            model = ollamaModel;
+        } else {
+            log.error("No such completion implemention supported yet: " + generateImpl);
+            model = null;
+        }
+        return model;
     }
 
     /**
