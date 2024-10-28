a96234dd21d35f2d652f725ac3867554d98e8270
Michael Wechner <michael.wechner@wyona.com>
2024-10-28 22:46:42 +0100

    conditional statement added

--- src/main/java/com/wyona/katie/health/AnswerFromTextServiceHealthIndicator.java
+++ src/main/java/com/wyona/katie/health/AnswerFromTextServiceHealthIndicator.java
@@ -4,15 +4,17 @@ import com.wyona.katie.services.AnswerFromTextServiceRestImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.stereotype.Component;
 
 /**
- * Also see configuration parameter 'management.endpoint.health.show-details' inside application.properties
+ * Also see configuration parameter 'management.health.answer-from-text-service.enabled' inside application.properties
  */
 @Slf4j
 @Component
+@ConditionalOnEnabledHealthIndicator("answer-from-text-service")
 public class AnswerFromTextServiceHealthIndicator extends AbstractHealthIndicator {
 
     @Value("${aft.rest.impl.host}")
@@ -27,14 +29,10 @@ public class AnswerFromTextServiceHealthIndicator extends AbstractHealthIndicato
         // If you throw an exception, the status will be DOWN with the exception message.
 
         String endpoint = "/api/v1/health";
-        if (false) { // TODO: Check whether service is enabled
-            if (answerFromTextServiceRest.isAlive(endpoint)) {
-                builder.up().withDetail("endpoint", endpoint).withDetail("host", host);
-            } else {
-                builder.down().withDetail("endpoint", endpoint).withDetail("host", host);
-            }
+        if (answerFromTextServiceRest.isAlive(endpoint)) {
+            builder.up().withDetail("endpoint", endpoint).withDetail("host", host);
         } else {
-            builder.unknown().withDetail("endpoint", endpoint).withDetail("host", host);
+            builder.down().withDetail("endpoint", endpoint).withDetail("host", host);
         }
     }
 }
