4e44a8cb974c549077ae49ca16b0cbdf61f5033b
Michael Wechner <michael.wechner@wyona.com>
2024-12-09 11:35:55 +0100

    description added

--- src/main/java/com/wyona/katie/models/Classification.java
+++ src/main/java/com/wyona/katie/models/Classification.java
@@ -3,6 +3,7 @@ package com.wyona.katie.models;
 public class Classification implements Comparable<Classification> {
 
     private String label;
+    private String description;
     private String id;
     private String katieId;
     private Integer frequency = null;
@@ -15,8 +16,8 @@ public class Classification implements Comparable<Classification> {
 
     /**
      * @param label Class name / Label, e.g. "Managed Device Services, MacOS Clients"
-     * @param id Foreign class Id, e.g. "64e3bb24-1522-4c49-8f82-f99b34a82062" or "https://jena.apache.org/2f61f866-bcd8-4db3-833b-37e6f7877e52"
-     * @param katieId Id assigned by Katie
+     * @param id Foreign class Id, e.g. "9daa804d-95dc-4a60-b1b6-995977607a4e_99e33f5d-37a1-4333-b4a9-483535fa187a" or "64e3bb24-1522-4c49-8f82-f99b34a82062" or "https://jena.apache.org/2f61f866-bcd8-4db3-833b-37e6f7877e52"
+     * @param katieId Id assigned by Katie, e.g. "eb818156-6c77-4673-b612-fc94b82bb1a8"
      */
     public Classification(String label, String id, String katieId) {
         this.label = label;
@@ -25,17 +26,31 @@ public class Classification implements Comparable<Classification> {
     }
 
     /**
-     * @param label Class name / Label
+     * @param label Class name / Label, e.g. "Managed Device Services (ZI), MacOS Clients"
      */
     public void setTerm(String label) {
         this.label = label;
     }
 
     /**
-     * @return class name / label
+     * @return class name / label, e.g. "Managed Device Services (ZI), MacOS Clients"
      */
     public String getTerm() {
         return label;
+    }
+
+    /**
+     * @param description Description of classification, e.g. "This subcategory contains incidents involving devices with MacOS that are managed by the University of Zurich."
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return description of classification, e.g. "This subcategory contains incidents involving devices with MacOS that are managed by the University of Zurich."
+     */
+    public String getDescription() {
+        return description;
     }
 
     /**
