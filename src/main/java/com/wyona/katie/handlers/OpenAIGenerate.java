6b21806b455988ca963b4ed5348f173580458439
Michael Wechner <michael.wechner@wyona.com>
2025-01-07 12:29:06 +0100

    assistant thread endpoint implementation started

--- src/main/java/com/wyona/katie/handlers/OpenAIGenerate.java
+++ src/main/java/com/wyona/katie/handlers/OpenAIGenerate.java
@@ -33,6 +33,17 @@ public class OpenAIGenerate implements GenerateProvider {
      * @see GenerateProvider#getCompletion(List, String, Double, String)
      */
     public String getCompletion(List<PromptMessage> promptMessages, String openAIModel, Double temperature, String openAIKey) throws Exception {
+        if (true) {
+            return chatCompletion(promptMessages, openAIModel, temperature, openAIKey);
+        } else {
+            return assistantThread(promptMessages, openAIModel, temperature, openAIKey);
+        }
+    }
+
+    /**
+     * Generate answer using OpenAI Chat Completion API https://platform.openai.com/docs/api-reference/chat
+     */
+    private String chatCompletion(List<PromptMessage> promptMessages, String openAIModel, Double temperature, String openAIKey) throws Exception {
         log.info("Complete prompt using OpenAI chat completion (API key: " + openAIKey.substring(0, 7) + "******) ...");
 
         String completedText = null;
@@ -75,12 +86,12 @@ public class OpenAIGenerate implements GenerateProvider {
                 propertyNode.put("type", "string");
             }
 
-            RestTemplate restTemplate = new RestTemplate();
             HttpHeaders headers = getHttpHeaders(openAIKey);
             HttpEntity<String> request = new HttpEntity<String>(requestBodyNode.toString(), headers);
 
             String requestUrl = openAIHost + "/v1/chat/completions";
             log.info("Get chat completion: " + requestUrl + " (Body: " + requestBodyNode + ")");
+            RestTemplate restTemplate = new RestTemplate();
             ResponseEntity<JsonNode> response = restTemplate.exchange(requestUrl, HttpMethod.POST, request, JsonNode.class);
             JsonNode responseBodyNode = response.getBody();
             log.info("JSON Response: " + responseBodyNode);
@@ -99,8 +110,67 @@ public class OpenAIGenerate implements GenerateProvider {
         return completedText;
     }
 
+    /***
+     * Generate answer using OpenAI Assistant Thread API https://platform.openai.com/docs/api-reference/threads
+     */
+    private String assistantThread(List<PromptMessage> promptMessages, String openAIModel, Double temperature, String openAIKey) throws Exception {
+        log.info("Complete prompt using OpenAI assistant thread (API key: " + openAIKey.substring(0, 7) + "******) ...");
+
+        String completedText = null;
+
+        try {
+            // INFO: See https://platform.openai.com/docs/api-reference/threads
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectNode requestBodyNode = mapper.createObjectNode();
+            //requestBodyNode.put("model", openAIModel);
+            //requestBodyNode.put("model", "gpt-4o-2024-08-06");
+            ArrayNode messages = mapper.createArrayNode();
+            requestBodyNode.put("messages", messages);
+
+            for (PromptMessage msg : promptMessages) {
+                ObjectNode messageNode = mapper.createObjectNode();
+                messageNode.put("role", msg.getRole().toString());
+                messageNode.put("content", msg.getContent());
+
+                String[] attachments = msg.getAttachments();
+                if (attachments != null && attachments.length > 0) {
+                    ArrayNode attachmentsNode = mapper.createArrayNode();
+                    messageNode.put("attachments", attachmentsNode);
+                    for (String attachment : attachments) {
+                        // TODO: Add attachment, see for example https://platform.openai.com/docs/assistants/tools/file-search
+                    }
+                }
+
+                messages.add(messageNode);
+            }
+
+            HttpHeaders headers = getHttpHeaders(openAIKey);
+            headers.set("OpenAI-Beta", "assistants=v2");
+            HttpEntity<String> request = new HttpEntity<String>(requestBodyNode.toString(), headers);
+
+            String requestUrl = openAIHost + "/v1/threads";
+            log.info("Get chat completion: " + requestUrl + " (Body: " + requestBodyNode + ")");
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<JsonNode> response = restTemplate.exchange(requestUrl, HttpMethod.POST, request, JsonNode.class);
+            JsonNode responseBodyNode = response.getBody();
+            log.info("JSON Response: " + responseBodyNode);
+
+            String threadId = responseBodyNode.get("id").asText();
+            if (threadId != null) {
+                completedText = "Thread ID: " + threadId;
+            } else {
+                log.warn("No thread Id!");
+            }
+        } catch(Exception e) {
+            log.error(e.getMessage(), e);
+            throw e;
+        }
+
+        return completedText;
+    }
+
     /**
-     *
+     * Get default headers
      */
     private HttpHeaders getHttpHeaders(String openAIKey) {
         HttpHeaders headers = new HttpHeaders();
