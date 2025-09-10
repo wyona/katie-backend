56a9a3bd67320dde482bbdfa237d49a1c282222a
Michael Wechner <michael.wechner@wyona.com>
2025-09-10 16:26:43 +0200

    use Lucene101Codec

--- src/main/java/com/wyona/katie/services/LuceneCodecFactory.java
+++ src/main/java/com/wyona/katie/services/LuceneCodecFactory.java
@@ -5,12 +5,10 @@ import org.apache.lucene.codecs.Codec;
 import org.apache.lucene.codecs.KnnVectorsFormat;
 import org.apache.lucene.codecs.KnnVectorsReader;
 import org.apache.lucene.codecs.KnnVectorsWriter;
-//import org.apache.lucene.backward_codecs.lucene95.Lucene95Codec;
-//import org.apache.lucene.backward_codecs.lucene95.Lucene95HnswVectorsFormat;
 import org.apache.lucene.backward_codecs.lucene99.Lucene99Codec;
-//import org.apache.lucene.codecs.lucene99.Lucene99Codec;
+import org.apache.lucene.codecs.lucene101.Lucene101Codec;
+import org.apache.lucene.codecs.lucene102.Lucene102HnswBinaryQuantizedVectorsFormat;
 import org.apache.lucene.codecs.lucene99.Lucene99HnswScalarQuantizedVectorsFormat;
-import org.apache.lucene.codecs.lucene99.Lucene99HnswVectorsFormat;
 import org.apache.lucene.index.SegmentReadState;
 import org.apache.lucene.index.SegmentWriteState;
 import org.springframework.stereotype.Component;
@@ -43,10 +41,10 @@ public class LuceneCodecFactory {
             };
             return codecInt8;
         } else {
-            Codec codecFloat32 = new Lucene99Codec() {
+            Codec codecFloat32 = new Lucene101Codec() {
                 @Override
                 public KnnVectorsFormat getKnnVectorsFormatForField(String field) {
-                    var delegate = new Lucene99HnswVectorsFormat();
+                    var delegate = new Lucene102HnswBinaryQuantizedVectorsFormat();
                     log.info("Vector Value Type: float32, Maximum Vector Dimension: " + maxDimensions);
                     return new DelegatingKnnVectorsFormat(delegate, maxDimensions);
                 }
