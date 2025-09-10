2c99b7e785e1fcf30a3f8df6e3de13d0baeb95c4
Michael Wechner <michael.wechner@wyona.com>
2025-09-10 16:50:24 +0200

    use Lucene101Codec

--- src/main/java/com/wyona/katie/services/LuceneCodecFactory.java
+++ src/main/java/com/wyona/katie/services/LuceneCodecFactory.java
@@ -5,10 +5,8 @@ import org.apache.lucene.codecs.Codec;
 import org.apache.lucene.codecs.KnnVectorsFormat;
 import org.apache.lucene.codecs.KnnVectorsReader;
 import org.apache.lucene.codecs.KnnVectorsWriter;
-import org.apache.lucene.backward_codecs.lucene99.Lucene99Codec;
 import org.apache.lucene.codecs.lucene101.Lucene101Codec;
 import org.apache.lucene.codecs.lucene102.Lucene102HnswBinaryQuantizedVectorsFormat;
-import org.apache.lucene.codecs.lucene99.Lucene99HnswScalarQuantizedVectorsFormat;
 import org.apache.lucene.index.SegmentReadState;
 import org.apache.lucene.index.SegmentWriteState;
 import org.springframework.stereotype.Component;
@@ -23,18 +21,19 @@ public class LuceneCodecFactory {
     private final int maxDimensions = 16384; // TODO: Make configurable
 
     /**
-     *
+     * Get Lucene codec
+     * @param valueType Value type, "float32" or "int8"
      */
     public Codec getCodec(EmbeddingValueType valueType) {
-        //return Lucene95Codec.getDefault();
+        //return Lucene101Codec.getDefault();
 
         log.info("Get codec ...");
 
         if (valueType == EmbeddingValueType.int8) {
-            Codec codecInt8 = new Lucene99Codec() {
+            Codec codecInt8 = new Lucene101Codec() {
                 @Override
                 public KnnVectorsFormat getKnnVectorsFormatForField(String field) {
-                    var delegate = new Lucene99HnswScalarQuantizedVectorsFormat();
+                    var delegate = new Lucene102HnswBinaryQuantizedVectorsFormat();
                     log.info("Vector Value Type: int8, Maximum Vector Dimension: " + maxDimensions);
                     return new DelegatingKnnVectorsFormat(delegate, maxDimensions);
                 }
@@ -44,6 +43,7 @@ public class LuceneCodecFactory {
             Codec codecFloat32 = new Lucene101Codec() {
                 @Override
                 public KnnVectorsFormat getKnnVectorsFormatForField(String field) {
+                    // TODO: Shouldn't there be a non quantized format version, similar to https://lucene.apache.org/core/9_12_0/core/org/apache/lucene/codecs/lucene99/Lucene99HnswVectorsFormat.html
                     var delegate = new Lucene102HnswBinaryQuantizedVectorsFormat();
                     log.info("Vector Value Type: float32, Maximum Vector Dimension: " + maxDimensions);
                     return new DelegatingKnnVectorsFormat(delegate, maxDimensions);
