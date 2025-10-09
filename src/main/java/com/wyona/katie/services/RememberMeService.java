19411e6637eb740a257f399597fb1efcded0864b
Michael Wechner <michael.wechner@wyona.com>
2025-10-09 11:03:44 -0300

    use jakarta servlet

--- src/main/java/com/wyona/katie/services/RememberMeService.java
+++ src/main/java/com/wyona/katie/services/RememberMeService.java
@@ -9,9 +9,9 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.UUID;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import lombok.extern.slf4j.Slf4j;
 
