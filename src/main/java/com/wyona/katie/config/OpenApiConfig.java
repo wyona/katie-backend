0676c68d0f942466ebc4c275701559a6b5f9b5c5
Michael Wechner <michael.wechner@wyona.com>
2025-10-09 13:39:34 -0300

    OpenApi config added

---
+++ src/main/java/com/wyona/katie/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.wyona.katie.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI apiV3() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Katie API")
+                        .version("3.0")
+                        .description("Documentation API V3")
+                        .contact(new Contact().name("Wyona").email("michael.wechner@wyona.com")))
+                ;
+    }
+}
