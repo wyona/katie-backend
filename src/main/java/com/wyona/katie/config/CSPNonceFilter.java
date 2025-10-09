0d40316fb21067df383782d69b306040bef890fa
Michael Wechner <michael.wechner@wyona.com>
2025-10-09 11:20:38 -0300

    use jakarta servlet

--- src/main/java/com/wyona/katie/config/CSPNonceFilter.java
+++ src/main/java/com/wyona/katie/config/CSPNonceFilter.java
@@ -1,8 +1,8 @@
 package com.wyona.katie.config;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
