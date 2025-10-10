479bae8dfa178b9e34445290aa7121c417cbb837
Michael Wechner <michael.wechner@wyona.com>
2025-10-10 15:16:03 -0300

    insert vector implemented

--- src/main/java/com/wyona/katie/handlers/MilvusRetrievalImpl.java
+++ src/main/java/com/wyona/katie/handlers/MilvusRetrievalImpl.java
@@ -1,5 +1,7 @@
 package com.wyona.katie.handlers;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.wyona.katie.models.*;
 
 import io.milvus.v2.client.ConnectConfig;
@@ -11,6 +13,8 @@ import io.milvus.v2.service.collection.request.CreateCollectionReq;
 
 import io.milvus.v2.service.collection.request.DropCollectionReq;
 import io.milvus.v2.service.collection.request.GetLoadStateReq;
+import io.milvus.v2.service.vector.request.InsertReq;
+import io.milvus.v2.service.vector.response.InsertResp;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +29,10 @@ public class MilvusRetrievalImpl implements QuestionAnswerHandler {
 
     @Value("${milvus.host}")
     private String milvusHostDefault;
+
+    private static final String UUID_FIELD = "uuid";
+    private static final String VECTOR_FIELD = "vector";
+    private static final String CLASSIFICATION_FIELD = "classification";
 
     /**
      * Check whether Weaviate is alive
@@ -63,36 +71,39 @@ public class MilvusRetrievalImpl implements QuestionAnswerHandler {
         log.info("Create Milvus collection ...");
         domain.setMilvusBaseUrl(getHost(null));
 
+        int VECTOR_DIMENSION = 5;
+
         MilvusClientV2 client = getMilvusClient(domain);
 
         try {
             // INFO: Create schema
             CreateCollectionReq.CollectionSchema schema = client.createSchema();
             schema.addField(AddFieldReq.builder()
-                    .fieldName("my_id")
-                    .dataType(DataType.Int64)
+                    .fieldName(UUID_FIELD)
+                    .dataType(DataType.VarChar)
+                    .maxLength(512)
                     .isPrimaryKey(true)
                     .autoID(false)
                     .build());
             schema.addField(AddFieldReq.builder()
-                    .fieldName("my_vector")
+                    .fieldName(VECTOR_FIELD)
                     .dataType(DataType.FloatVector)
-                    .dimension(5)
+                    .dimension(VECTOR_DIMENSION)
                     .build());
             schema.addField(AddFieldReq.builder()
-                    .fieldName("my_varchar")
+                    .fieldName(CLASSIFICATION_FIELD)
                     .dataType(DataType.VarChar)
                     .maxLength(512)
                     .build());
 
             // INFO: Set index parameters
             IndexParam indexParamForIdField = IndexParam.builder()
-                    .fieldName("my_id")
+                    .fieldName(UUID_FIELD)
                     .indexType(IndexParam.IndexType.AUTOINDEX)
                     .build();
 
             IndexParam indexParamForVectorField = IndexParam.builder()
-                    .fieldName("my_vector")
+                    .fieldName(VECTOR_FIELD)
                     .indexType(IndexParam.IndexType.AUTOINDEX)
                     //.metricType(IndexParam.MetricType.COSINE)
                     .metricType(IndexParam.MetricType.L2)
@@ -130,6 +141,27 @@ public class MilvusRetrievalImpl implements QuestionAnswerHandler {
      */
     public void train(QnA qna, Context domain, boolean indexAlternativeQuestions) {
         log.error("TODO: Implement method train()!");
+
+        MilvusClientV2 client = getMilvusClient(domain);
+        String collectionName = getCollectionName(domain);
+        try {
+            qna.getClassifications();
+            String classification = "TODO";
+            String[] embedding = null; // TODO
+            Gson gson = new Gson();
+            List<JsonObject> data = Arrays.asList(
+                    gson.fromJson("{\"" + UUID_FIELD + "\": \"" + qna.getUuid() + "\", \"" + VECTOR_FIELD + "\": [0.3580376395471989, -0.6023495712049978, 0.18414012509913835, -0.26286205330961354, 0.9029438446296592], \"" + CLASSIFICATION_FIELD + "\": \"" + classification + "\"}", JsonObject.class)
+            );
+            InsertReq insertReq = InsertReq.builder()
+                    .collectionName(collectionName)
+                    .data(data)
+                    .build();
+
+            InsertResp insertResp = client.insert(insertReq);
+            log.info("Milvus insert response: " + insertResp);
+        } finally {
+            client.close();
+        }
     }
 
     /**
@@ -163,7 +195,10 @@ public class MilvusRetrievalImpl implements QuestionAnswerHandler {
      */
     public boolean delete(String uuid, Context domain) {
         log.error("TODO: Implement method delete()!");
-        return false;
+
+        // TODO
+
+        return true;
     }
 
     /**
