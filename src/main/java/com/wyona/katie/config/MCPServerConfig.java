2137ca76350dc7f4d2b307dbb8e1cb963d372604
Michael Wechner <michael.wechner@wyona.com>
2025-10-25 22:19:00 +0200

    use ToolCallbackProvider

--- src/main/java/com/wyona/katie/config/MCPServerConfig.java
+++ src/main/java/com/wyona/katie/config/MCPServerConfig.java
@@ -1,18 +1,32 @@
 package com.wyona.katie.config;
 
 import com.wyona.katie.services.MCPRetrievalService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.support.ToolCallbacks;
 import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Configuration
+@Slf4j
 public class MCPServerConfig {
 
+    /*
     @Bean
-    public List<ToolCallback> findTools(MCPRetrievalService retrievalService) {
+    public List<ToolCallback> retrievalTools(MCPRetrievalService retrievalService) {
         return List.of(ToolCallbacks.from(retrievalService));
+    }
+    */
+
+    @Bean
+    ToolCallbackProvider retrievalTools(MCPRetrievalService retrievalService) {
+        log.info("Get all MCP retrieval tools ...");
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(retrievalService)
+                .build();
     }
 }
