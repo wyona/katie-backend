041d68055fad2c676ffd71a2fa9fd789c96b1e0b
Michael Wechner <michael.wechner@wyona.com>
2025-10-25 20:15:38 +0200

    keyword added

--- src/main/java/com/wyona/katie/services/MCPRetrievalService.java
+++ src/main/java/com/wyona/katie/services/MCPRetrievalService.java
@@ -2,22 +2,25 @@ package com.wyona.katie.services;
 
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.stereotype.Service;
-
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Service
 public class MCPRetrievalService {
 
-    @Tool(name = "katie_text_search", description = "Find relevant content")
-    public List<String> findAll() {
-        log.info("Finding relevant content ...");
-        List<String> relevantContent = new ArrayList<>();
-        relevantContent.add("Katharina was born October 18, 1896");
-        relevantContent.add("Michael was born February 16, 1969");
-        return relevantContent;
+    @Tool(
+            name = "katie_text_search",
+            description = "Find relevant content by keyword"
+    )
+    public List<String> findRelevantContent(String keyword) {
+        log.info("Finding relevant content for query: {}", keyword);
+
+        return List.of(
+                "Katharina was born October 18, 1896",
+                "Michael was born February 16, 1969",
+                "Result for keyword: " + keyword
+        );
     }
 }
