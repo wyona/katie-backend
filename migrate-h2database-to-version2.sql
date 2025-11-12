bd39d6bf1db2a039dfdd6be9ba013e930bcc7d07
Michael Wechner <michael.wechner@wyona.com>
2025-11-12 13:16:21 +0100

    typo fixed

--- migrate-h2database-to-version2.sql
+++ migrate-h2database-to-version2.sql
@@ -10,5 +10,5 @@ KATIE_VOLUME=/Users/michaelwechner/src/wyona/public/katie-backend/volume
 echo "Dump database as SQL file ..."
 java -cp $MAVEN_REPO_H2/1.4.197/h2-1.4.197.jar org.h2.tools.Script -url jdbc:h2:$KATIE_VOLUME/askkatie-h2 -user $USERNAME -password $PASSWORD -script $SQL_FILE
 
-echo "Recrate database version 2 from SQL file ..."
+echo "Recreate database version 2 from SQL file ..."
 java -cp $MAVEN_REPO_H2/2.4.240/h2-2.4.240.jar org.h2.tools.RunScript -url jdbc:h2:$KATIE_VOLUME/askkatie-h2-v2 -user $USERNAME -password $PASSWORD -script $SQL_FILE
