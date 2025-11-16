96594402481d5610a73334df738f61cfe3748d0e
Michael Wechner <michael.wechner@wyona.com>
2025-11-16 15:57:22 +0100

    modify replaced by alter column

--- src/main/resources/db/migration/V20_3__alter_table_slack_team_katie_domain.sql
+++ src/main/resources/db/migration/V20_3__alter_table_slack_team_katie_domain.sql
@@ -1,2 +1,2 @@
 /* Change size of approval token column */
-alter table SLACK_TEAM_KATIE_DOMAIN modify APPROVAL_TOKEN varchar(500);
+alter table SLACK_TEAM_KATIE_DOMAIN alter column APPROVAL_TOKEN varchar(500);
--- src/main/resources/db/migration/V26_1__resize_question_field.sql
+++ src/main/resources/db/migration/V26_1__resize_question_field.sql
@@ -1,3 +1,3 @@
 /* Change size of question column */
-alter table RESUBMITTED_QUESTION modify QUESTION varchar(2000);
-alter table QUESTION modify QUESTION varchar(2000);
+alter table RESUBMITTED_QUESTION alter column QUESTION varchar(2000);
+alter table QUESTION alter column QUESTION varchar(2000);
