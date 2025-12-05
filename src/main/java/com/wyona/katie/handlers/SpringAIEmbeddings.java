a65e62423c37a26960e13ef78c7aae0bd3803b77
Michael Wechner <michael.wechner@wyona.com>
2025-12-05 13:32:57 +0100

    obsolete imports removed

--- src/main/java/com/wyona/katie/handlers/SpringAIEmbeddings.java
+++ src/main/java/com/wyona/katie/handlers/SpringAIEmbeddings.java
@@ -1,37 +1,19 @@
 package com.wyona.katie.handlers;
 
 import com.wyona.katie.models.Vector;
-import com.wyona.katie.models.ByteVector;
 import com.wyona.katie.models.EmbeddingType;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.wyona.katie.models.EmbeddingValueType;
 import com.wyona.katie.models.FloatVector;
-import io.micrometer.observation.ObservationRegistry;
 import org.springframework.ai.embedding.Embedding;
-import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.embedding.EmbeddingRequest;
 import org.springframework.ai.embedding.EmbeddingResponse;
 import org.springframework.ai.ollama.OllamaEmbeddingModel;
-import org.springframework.ai.ollama.api.OllamaApi;
 import org.springframework.ai.ollama.api.OllamaEmbeddingOptions;
-import org.springframework.ai.ollama.api.OllamaModel;
-import org.springframework.ai.ollama.management.ModelManagementOptions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
-import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.List;
 
