724021096a493093fbe43874e30064805a0110bd
Michael Wechner <michael.wechner@wyona.com>
2025-12-13 11:07:40 +0100

    Azure implementation started

---
+++ src/main/java/com/wyona/katie/handlers/AzureGenerate.java
@@ -0,0 +1,30 @@
+package com.wyona.katie.handlers;
+
+import com.wyona.katie.models.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ *
+ */
+@Slf4j
+@Component
+public class AzureGenerate implements GenerateProvider {
+
+    /**
+     * @see GenerateProvider#getAssistant(String, String, String, List, CompletionConfig)
+     */
+    public CompletionAssistant getAssistant(String id, String name, String instructions, List<CompletionTool> tools, CompletionConfig completionConfig) throws Exception {
+        log.error("Not implemented yet!");
+        return null;
+    }
+
+    /**
+     * @see GenerateProvider#getCompletion(List, CompletionAssistant, CompletionConfig, Double)
+     */
+    public CompletionResponse getCompletion(List<PromptMessage> promptMessages, CompletionAssistant assistant, CompletionConfig completionConfig, Double temperature) throws Exception {
+        return null;
+    }
+}
