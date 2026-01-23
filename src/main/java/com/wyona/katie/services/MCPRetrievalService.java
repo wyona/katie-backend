6128870d074f62613e81c6a5a576bb8a40538289
Michael Wechner <michael.wechner@wyona.com>
2026-01-23 12:16:32 +0100

    MCP tool to get dates for the city of Zurich’s paper and cardboard collection

--- src/main/java/com/wyona/katie/services/MCPRetrievalService.java
+++ src/main/java/com/wyona/katie/services/MCPRetrievalService.java
@@ -28,5 +28,27 @@ public class MCPRetrievalService {
                 "Result for question: " + question
         );
     }
+
+    /**
+     * Get the dates for the city of Zurich’s paper and cardboard collection.
+     * @param zipCode Swiss ZIP code within the city of Zurich (e.g. 8044, 8003, 8032).
+     * @return A list of upcoming paper and cardboard collection dates
+     */
+    @Tool(
+            name = "katie_paper_cardboard_collection_city_of_zurich",
+            description = "Get the dates for the city of Zurich’s paper and cardboard collection."
+    )
+    public List<String> getPapierKartonSammlungDatesCityOfZurich(
+            @ToolParam(description = "Swiss ZIP code within the city of Zurich (e.g. 8044, 8003, 8032).", required = true) Integer zipCode
+
+    ) {
+        log.info("Get the paper and cardboard collection dates for ZIP code " + zipCode + " in the city of Zurich.");
+
+        return List.of(
+                "26. Januar 2026",
+                "09. Februar 2026",
+                "23. Februar 2026"
+        );
+    }
 }
 
