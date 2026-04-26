13fa56f12ad28bfcc2e8c060b7e444f2bfbcdd3e
Michael Wechner <michael.wechner@wyona.com>
2026-04-26 21:15:54 +0200

    check access token

--- src/main/java/com/wyona/katie/services/MCPRetrievalService.java
+++ src/main/java/com/wyona/katie/services/MCPRetrievalService.java
@@ -3,6 +3,11 @@ package com.wyona.katie.services;
 import com.wyona.katie.answers.OpenERZ;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,6 +19,9 @@ import java.util.List;
 @Service
 public class MCPRetrievalService {
 
+    @Autowired
+    JwtService jwtService;
+
     @Tool(
             name = "katie_text_search",
             description = "Find relevant content by natural language query"
@@ -21,7 +29,33 @@ public class MCPRetrievalService {
     public List<String> findRelevantContent(
             @ToolParam(description = "The question to search for", required = true) String question
             //@ToolParam(description = "The Katie knowledge base Id", required = false) String domainId
-    ) {
+    ) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof BearerTokenAuthenticationToken) {
+            BearerTokenAuthenticationToken bearerToken = (BearerTokenAuthenticationToken) authentication;
+            //log.debug("Bearer token: " + bearerToken.getToken());
+            if (jwtService.isJWTValid(bearerToken.getToken(), null)) {
+                log.info("Bearer Token is valid");
+            } else {
+                log.warn("Bearer Token is not valid!");
+                throw new Exception("Bearer token invalid!");
+            }
+        } else if (authentication instanceof JwtAuthenticationToken) {
+            JwtAuthenticationToken jwtToken = (JwtAuthenticationToken) authentication;
+            //log.debug("JWT token: " + jwtToken.getToken().getTokenValue());
+            if (jwtService.isJWTValid(jwtToken.getToken().getTokenValue(), null)) {
+                log.info("JWT Token is valid");
+            } else {
+                log.warn("JWT Token is not valid!");
+                throw new Exception("Access token invalid!");
+            }
+        } else {
+            //Object principal = authentication.getPrincipal();
+            //log.info("Authentication principal : " + principal);
+            log.warn("Authentication " + authentication + " not implemented!");
+            throw new Exception("Authentication " + authentication + " not implemented!");
+        }
+
         String domainId = "TODO";
         log.info("Finding relevant content for question '" + question + "' inside domain '" + domainId + "' ...");
 
