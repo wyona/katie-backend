1f73c9dd42cfc752686cce2726d334822294d707
Michael Wechner <michael.wechner@wyona.com>
2026-06-08 15:47:35 +0200

    filesystem connector started

---
+++ src/main/java/com/wyona/katie/connectors/FilesystemConnector.java
@@ -0,0 +1,43 @@
+package com.wyona.katie.connectors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.wyona.katie.models.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Connector for ingesting data from Filesystem
+ */
+@Slf4j
+@Component
+public class FilesystemConnector implements Connector {
+
+    /**
+     * @see Connector#getAnswers(Sentence, int, KnowledgeSourceMeta)
+     */
+    public Hit[] getAnswers(Sentence question, int limit, KnowledgeSourceMeta ksMeta) {
+        log.info("Get answers using third-party RAG connector '" + ksMeta.getName() + "' ...");
+        List<Hit> hits = new ArrayList<Hit>();
+
+        return hits.toArray(new Hit[0]);
+    }
+
+    /**
+     * @see Connector#update(Context, KnowledgeSourceMeta, WebhookPayload, String)
+     */
+    public List<Answer> update(Context domain, KnowledgeSourceMeta ksMeta, WebhookPayload payload, String processId) {
+        log.info("TODO: Implement");
+        return null;
+    }
+}
