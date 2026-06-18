3dec412080e7a84dd75a98a26b3fef78039b5e62
Michael Wechner <michael.wechner@wyona.com>
2026-06-18 15:08:48 +0200

    duplicated semi-colon removed

--- src/main/java/com/wyona/katie/config/SecurityConfig.java
+++ src/main/java/com/wyona/katie/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
                 .requestMatchers(GET, "/tmp-test/push-notification/sample").hasRole(ADMIN_ROLE)
                 //.requestMatchers("/mcp/**").authenticated()
                 .anyRequest().permitAll()
-        ).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));;
+        ).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
         // https://www.baeldung.com/spring-security-logout
         http.logout(logout -> logout
