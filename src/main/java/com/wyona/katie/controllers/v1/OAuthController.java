c394b8d8e8e97405ab4ef4f7767739a63c0f41b3
Michael Wechner <michael.wechner@wyona.com>
2026-06-22 16:22:31 +0200

    get token implemented

--- src/main/java/com/wyona/katie/controllers/v1/OAuthController.java
+++ src/main/java/com/wyona/katie/controllers/v1/OAuthController.java
@@ -1,7 +1,10 @@
 package com.wyona.katie.controllers.v1;
 
+import com.wyona.katie.services.AuthenticationService;
+import com.wyona.katie.services.JwtService;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +27,14 @@ import jakarta.servlet.http.HttpServletResponse;
 @RestController
 public class OAuthController {
 
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    private String TODO_CODE = "TODO_CODE";
+
     /**
      * TODO
      */
@@ -44,20 +55,57 @@ public class OAuthController {
         log.info("Response type: " + responseType);
         log.info("Redirect URI: " + redirectUri);
 
-        String scope = "mcp";
-        String once = "todo_once";
-
-        String googleOAuthUrl = "https://accounts.google.com/o/oauth2/v2/auth?client_id=" + clientId + "&response_type=" + responseType + "&scope=" + scope + "&redirect_uri=" + redirectUri + "&state=" + state + "&nonce=" + once;
-        boolean authenticated = true;
+        boolean authenticated = authenticationService.userIsSignedInBySession(request);
         if (!authenticated) {
+            String scope = "mcp";
+            String once = "todo_once";
+            String googleOAuthUrl = "https://accounts.google.com/o/oauth2/v2/auth?client_id=" + clientId + "&response_type=" + responseType + "&scope=" + scope + "&redirect_uri=" + redirectUri + "&state=" + state + "&nonce=" + once;
             redirectUri = googleOAuthUrl;
         } else {
             log.info("Already authenticated successfully.");
-            redirectUri = redirectUri + "?code=HUGO";
+            redirectUri = redirectUri + "?code=" + TODO_CODE;
         }
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", redirectUri);
         return new ResponseEntity<>(headers, HttpStatus.TEMPORARY_REDIRECT);
+    }
+
+    /**
+     * TODO
+     */
+    @PostMapping(value = "/token",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary="TODO")
+    public ResponseEntity<?> getToken(
+            @Parameter(name = "grant_type", description = "TODO", required = false)
+            @RequestParam(value = "grant_type", required = false) String grantType,
+            @Parameter(name = "redirect_urli", description = "Redirect URI", required = false)
+            @RequestParam(value = "redirect_uri", required = false) String redirectUri,
+            @Parameter(name = "code", description = "TODO", required = false)
+            @RequestParam(value = "code", required = false) String code,
+            @Parameter(name = "code_verifier", description = "TODO", required = false)
+            @RequestParam(value = "code_verifier", required = false) String codeVerifier,
+            @Parameter(name = "client_id", description = "Client Id, e.g, 1045897086839-7dhg0h1rbc9kdeklfdghtfj9r85p08dj.apps.googleusercontent.com", required = false)
+            @RequestParam(value = "client_id", required = false) String clientId,
+            HttpServletRequest request,
+            HttpServletResponse response) throws Exception  {
+
+        log.info("Verify code: " + code);
+        if (!code.equals(TODO_CODE)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        long seconds = 3600;
+        StringBuilder body = new StringBuilder("{");
+        body.append("\"token_type\":\"bearer\"");
+        String username = authenticationService.getUsername();
+        log.info("Generate access token for user '" + username + "' ...");
+        String domainId = "todo_domain_id";
+        String token = jwtService.generateJWT(username, domainId, seconds, null);
+        body.append(",\"access_token\":\"" + token + "\"");
+        body.append("}");
+
+        return new ResponseEntity<>(body.toString(), HttpStatus.OK);
     }
 }
