e3cf6b213d6d668c394e6c5436cc04f932d1817e
Michael Wechner <michael.wechner@wyona.com>
2026-06-24 14:16:24 +0200

    more fields added

--- src/main/java/com/wyona/katie/models/OAuthRegisterBody.java
+++ src/main/java/com/wyona/katie/models/OAuthRegisterBody.java
@@ -7,6 +7,9 @@ public class OAuthRegisterBody {
 
     private String clientName;
     private String[] redirectUris;
+    private String[] grantTypes;
+    private String[] responseTypes;
+    private String tokenEndpointAuthMethod;
 
     /**
      *
@@ -34,5 +37,47 @@ public class OAuthRegisterBody {
      */
     public String[] getRedirect_uris() {
         return redirectUris;
+    }
+
+    /**
+     *
+     */
+    public void setGrant_types(String[] grantTypes) {
+        this.grantTypes = grantTypes;
+    }
+
+    /**
+     *
+     */
+    public String[] getGrant_types() {
+        return grantTypes;
+    }
+
+    /**
+     *
+     */
+    public void setResponse_types(String[] responseTypes) {
+        this.responseTypes = responseTypes;
+    }
+
+    /**
+     *
+     */
+    public String[] getResponse_types() {
+        return responseTypes;
+    }
+
+    /**
+     * @param tokenEndpointAuthMethod Token endpoint auth method, e.g., "client_secret_basic"
+     */
+    public void setToken_endpoint_auth_method(String tokenEndpointAuthMethod) {
+        this.tokenEndpointAuthMethod = tokenEndpointAuthMethod;
+    }
+
+    /**
+     *
+     */
+    public String getToken_endpoint_auth_method() {
+        return tokenEndpointAuthMethod;
     }
 }
