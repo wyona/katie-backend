d99e72c449dda087967ab6d21202a74097aeac91
Michael Wechner <michael.wechner@wyona.com>
2026-08-10 11:16:43 +0200

    obsolete imports removed

--- src/main/java/com/wyona/katie/controllers/v1/AuthenticationController.java
+++ src/main/java/com/wyona/katie/controllers/v1/AuthenticationController.java
@@ -13,11 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
