7d1bf8a4770c419242e8d6d1668b956172dba198
Michael Wechner <michael.wechner@wyona.com>
2026-09-09 15:20:01 +0200

    tag added

--- src/main/java/com/wyona/katie/controllers/v1/IndexPageController.java
+++ src/main/java/com/wyona/katie/controllers/v1/IndexPageController.java
@@ -1,11 +1,16 @@
 package com.wyona.katie.controllers.v1;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Index page controller
+ */
 @Controller
+@Tag(name = "Index Page Controller v1", description = "Endpoint for root URL '/'")
 @Slf4j
 public class IndexPageController {
 
